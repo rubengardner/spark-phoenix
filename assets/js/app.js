cc00f20fc1a869668bc97dd07f37c8ba663817d7
rubengardner <rgardnercuesta@gmail.com>
2025-10-24 10:21:08 +0200

    Refactor front end

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,8 @@
-import "../css/app.css";
+// We import the CSS which is extracted to its own file by esbuild.
+import "../css/app.css"
 
-import "phoenix_html";
-import { Socket } from "phoenix";
-import { LiveSocket } from "phoenix_live_view";
-import topbar from "../vendor/topbar";
+// Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
+import "phoenix_html"
 
-import "./board.js";
-
-let csrfToken = document
-  .querySelector("meta[name='csrf-token']")
-  .getAttribute("content");
-let liveSocket = new LiveSocket("/live", Socket, {
-  params: { _csrf_token: csrfToken },
-});
-
-topbar.config({ barColors: { 0: "#29d" }, shadowColor: "rgba(0, 0, 0, .3)" });
-window.addEventListener("phx:page-loading-start", (_info) => topbar.show());
-window.addEventListener("phx:page-loading-stop", (_info) => topbar.hide());
-
-liveSocket.connect();
-
-window.liveSocket = liveSocket;
+// Import our custom board logic
+import "./board.js"
